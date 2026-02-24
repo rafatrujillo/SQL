2d0cafb0497609f6ab85721f312e742522f45f7d
rafatrujillo <profissional.trujillo@outlook.com>
2026-02-24 10:19:09 -0300

    Finalizando exercicios

--- EXERCICIOS SITUACIONAIS WHERE.sql
+++ EXERCICIOS SITUACIONAIS WHERE.sql
@@ -38,6 +38,64 @@ SELECT
 	EmployeeCount AS 'Numero de funcionarios'
 FROM
 	DimStore
-Where Status = 'On'
+Where Status = 'On' AND StoreType = 'Store'
 
 -- Exercicio 05
+
+SELECT
+	*
+FROM
+	DimProduct
+WHERE ProductName LIKE '%Home Theater%' AND BrandName = 'Litware' AND AvailableForSaleDate = '20090315' /* Sem tracos pq a coluna e datetime */
+
+-- Exercicio 06
+
+SELECT
+	*
+FROM
+	DimStore
+WHERE Status = 'Off' AND StoreType = 'Store' /* Utilizando coluna Status*/
+
+SELECT
+	*
+FROM
+	DimStore
+WHERE CloseDate IS NOT NULL /* Utilizando outra coluna, mas dando mesmo resultado */
+
+-- Exercicio 07
+
+SELECT
+	StoreName,
+	EmployeeCount
+FROM
+	DimStore
+WHERE Status = 'On'
+ORDER BY EmployeeCount ASC /* DA PARA UTILIZAR O WHERE BETWEEN */
+
+-- Exercicio 08
+
+
+SELECT
+	ProductKey AS 'ID',
+	ProductName AS 'Nome do Produto',
+	UnitPrice AS 'Preco'
+FROM
+	DimProduct
+WHERE ProductDescription LIKE '%LCD%'
+
+-- EXERCICIO 09
+
+SELECT
+	*
+FROM
+	DimProduct
+WHERE ColorName IN ('Green', 'Orange', 'Black', 'Silver', 'Pink') AND BrandName IN ('Contoso', 'Litware', 'Fabrikam')
+
+-- EXERCICIO 10
+
+SELECT
+	*
+FROM
+	DimProduct
+WHERE BrandName = 'Contoso' AND ColorName = 'Silver' AND Weight BETWEEN 10 AND 30
+ORDER BY UnitPrice DESC
