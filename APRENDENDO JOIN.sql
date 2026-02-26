4621178a160f4e5745eb9cbae3da17232ef73b11
rafatrujillo <profissional.trujillo@outlook.com>
2026-02-26 10:58:57 -0300

    Aprendendo JOIN

--- APRENDENDO JOIN.sql
+++ APRENDENDO JOIN.sql
@@ -1,11 +1,109 @@
 -- APRENDENDO JOIN
 
-SELECT TOP(1000) * FROM FactSales
-SELECT * FROM DimChannel
+-- LEFT OUTER JOIN
+
+SELECT * FROM DimProduct
+SELECT * FROM DimProductSubcategory
 
 SELECT
-	ChannelKey,
-	SUM(SalesQuantity)
+	DimProduct.ProductKey,
+	DimProduct.ProductName,
+	DimProduct.ProductSubcategoryKey,
+	DimProductSubcategory.ProductSubcategoryName
 FROM
-	FactSales
-GROUP BY channelKey
+	DimProduct
+LEFT JOIN DimProductSubcategory
+	ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductSubcategoryKey
+
+-- RIGHT OUTER JOIN
+
+SELECT * FROM DimProduct
+SELECT * FROM DimProductSubcategory
+
+SELECT
+	DimProduct.ProductKey,
+	DimProduct.ProductName,
+	DimProduct.ProductSubcategoryKey,
+	DimProductSubcategory.ProductSubcategoryName
+FROM
+	DimProduct
+RIGHT JOIN DimProductSubcategory
+	ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductSubcategoryKey
+
+-- INNER JOIN
+
+SELECT * FROM DimProduct
+SELECT * FROM DimProductSubcategory
+
+SELECT
+	DimProduct.ProductKey,
+	DimProduct.ProductName,
+	DimProduct.ProductSubcategoryKey,
+	DimProductSubcategory.ProductSubcategoryName
+FROM
+	DimProduct
+INNER JOIN DimProductSubcategory
+	ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductCategoryKey
+
+-- FULL OUTER JOIN
+
+SELECT * FROM DimProduct
+SELECT * FROM DimProductSubcategory
+
+SELECT
+	DimProduct.ProductKey,
+	DimProduct.ProductName,
+	DimProduct.ProductSubcategoryKey,
+	DimProductSubcategory.ProductSubcategoryName
+FROM
+	DimProduct
+FULL JOIN DimProductSubcategory
+	ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductCategoryKey
+
+-- LEFT ANTI JOIN
+
+SELECT * FROM DimProduct
+SELECT * FROM DimProductSubcategory
+
+SELECT
+	DimProduct.ProductKey,
+	DimProduct.ProductName,
+	DimProduct.ProductSubcategoryKey,
+	DimProductSubcategory.ProductSubcategoryName
+FROM
+	DimProduct
+LEFT JOIN DimProductSubcategory
+	ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductCategoryKey
+WHERE ProductSubcategoryName IS NULL
+
+-- RIGHT ANTI JOIN
+
+SELECT * FROM DimProduct
+SELECT * FROM DimProductSubcategory
+
+SELECT
+	DimProduct.ProductKey,
+	DimProduct.ProductName,
+	DimProduct.ProductSubcategoryKey,
+	DimProductSubcategory.ProductSubcategoryName
+FROM
+	DimProduct
+RIGHT JOIN DimProductSubcategory
+	ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductCategoryKey
+WHERE ProductName IS NULL
+
+-- FULL ANTI JOIN
+
+SELECT * FROM DimProduct
+SELECT * FROM DimProductSubcategory
+
+SELECT
+	DimProduct.ProductKey,
+	DimProduct.ProductName,
+	DimProduct.ProductSubcategoryKey,
+	DimProductSubcategory.ProductSubcategoryName
+FROM
+	DimProduct
+FULL JOIN DimProductSubcategory
+	ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductCategoryKey
+WHERE ProductKey IS NULL OR ProductSubcategoryName IS NULL
