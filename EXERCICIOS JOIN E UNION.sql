44adb54b30696b209968a3403c63fb67643f80ca
rafatrujillo <profissional.trujillo@outlook.com>
2026-02-27 11:46:08 -0300

    Atualizacoes exercicios

--- EXERCICIOS JOIN E UNION.sql
+++ EXERCICIOS JOIN E UNION.sql
@@ -16,3 +16,53 @@ INNER JOIN DimProductSubcategory
 
 -- EXERCICIO 2
 
+SELECT * FROM DimProductSubcategory
+SELECT * FROM DimProductCategory
+
+SELECT
+	ProductSubcategoryKey,
+	ProductSubcategoryName,
+	DimProductSubcategory.ProductCategoryKey,
+	ProductCategoryName
+FROM
+	DimProductSubcategory
+LEFT JOIN DimProductCategory
+	ON DimProductSubcategory.ProductCategoryKey = DimProductCategory.ProductCategoryKey
+
+-- EXERCICIO 3
+
+SELECT * FROM DimStore
+SELECT * FROM DimGeography
+
+SELECT
+	StoreKey,
+	StoreName,
+	EmployeeCount,
+	ContinentName,
+	RegionCountryName
+FROM 
+	DimStore
+LEFT JOIN DimGeography
+	ON DimStore.GeographyKey = DimGeography.GeographyKey
+
+-- EXERCICIO 4
+
+SELECT * FROM DimProduct
+SELECT * FROM DimProductSubcategory
+SELECT * FROM DimProductCategory
+
+SELECT
+	ProductKey,
+	ProductName,
+	ProductDescription,
+	BrandName,
+	ProductCategoryDescription
+FROM
+	DimProduct
+LEFT JOIN DimProductSubcategory
+	ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductSubcategoryKey
+	LEFT JOIN DimProductCategory
+		ON DimProductSubcategory.ProductCategoryKey = DimProductCategory.ProductCategorykey
+
+-- EXERCICIO 5
+
